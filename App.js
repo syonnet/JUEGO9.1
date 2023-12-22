10a0bc2266d6b77436dee734d12d72c932243bf7
vic_gen <vic_gen@hotmail.com>
2023-12-22 08:51:05 -0500

    comments

--- App.js
+++ App.js
@@ -1,12 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
-import AppNavigator from './navigation/Navigation'; // Asegúrate de que la ruta sea correcta
+import AppNavigator from './navigation/Navigation';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import GameScreen from './screens/GameScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import { NavigationContainer } from '@react-navigation/native';
-import ScoresScreen from './screens/ScoresScreen';
 import { loadFonts } from './components/fonts';
 
 
@@ -29,15 +25,11 @@ const App = () => {
 
 
   return (
-      //<NavigationContainer>
       <SafeAreaProvider>
         <PaperProvider >
           <AppNavigator />
-          {/* <GameScreen/> */}
-          {/* <ScoresScreen /> */}
         </PaperProvider>
       </SafeAreaProvider>
-     //</NavigationContainer>
   );
 };
 
